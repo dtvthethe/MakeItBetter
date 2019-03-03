baaa4936cd68ad4427e66e269809786850482f83
Đàm Thế <damthe@ams-MacBook-Pro.local>
2019-03-03 14:33:17 +0700

    BT 1

---
+++ Foundation/day1/main.py
@@ -0,0 +1,20 @@
+# bai 1:
+# import random
+#
+# greeting_text = random.choice(['Xin chao', 'Hello', 'Hi', 'What"s up'])
+# print(greeting_text)
+
+#bai 2:
+k = 8
+for i in range(0,k):
+    for j in range(0, k - i -1):
+        print('   ', end="")
+    for z in range(0, i*2 - 1):
+        print(' * ', end="")
+    print('\n')
+
+'''
+  *   2
+ ***  1
+***** 0
+'''
