33226c03e4a5ce2f6da82f8a71fea13930d6dea1
Đàm Thế <damthe@ams-MacBook-Pro.local>
2019-04-20 11:56:39 +0700

    homework day 10

---
+++ Foundation/day10/main.py
@@ -0,0 +1,29 @@
+import os
+from PIL import Image
+
+str_img = 'data.PNG'
+
+# Create Image contain 'X' shape:
+def create_image_X_shape(file_name, rgb_point, width_height = 1):
+    img = Image.new('RGB', (width_height, width_height), (255, 255, 255))
+    for i in range(0, width_height):
+        for j in range(0, width_height):
+            if ((i == j) or j == (width_height - i - 1)): img.putpixel((i, j), rgb_point)
+    img.save(file_name, 'PNG')
+    img.close()
+
+# Get number of pixel by RGB
+def counter_color_by_pixel(file_name, rgb_point):
+    counter = 0
+    img = Image.open(file_name)
+    for pixel in img.getdata():
+        if pixel == rgb_point: counter += 1
+    return counter
+
+
+if(os.path.exists(str_img)):
+    print(counter_color_by_pixel(str_img, (255, 0, 0)))
+else:
+    create_image_X_shape(8, (255, 0, 0), str_img)
+
+
